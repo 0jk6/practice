d352d5f65b5b92aea5bb72272d2b4f9d388cc0fc
jaychandra6 <jaypatel6814@gmail.com>
2021-03-13 14:38:13 +0530

    added a bigger array as input

--- stack/RainWaterTrapping.cpp
+++ stack/RainWaterTrapping.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-	int arr[6] = {3,0,0,2,0,4};
-	int size = 6;
+	int arr[12] = {0,1,0,2,1,0,1,3,2,1,2,1};
+	int size = 12;
 
 	int maxL[size], maxR[size], water[size];
 
